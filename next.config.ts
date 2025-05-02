9dcd477a2d1bdd5fede0c8e3f1fb49fa5a90caa0
sidharth-gatoes <sidharth@gatoes.in>
2025-05-02 12:08:06 +0530

    test CICD pipeline

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:'export'
+  output:'export',
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
