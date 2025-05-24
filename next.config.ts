4d95ddbd3c4847007ac87bf3582c1c36d0fec776
sidharth-gatoes <sidharth@gatoes.in>
2025-05-24 10:13:28 +0530

    fix: service navigation issue

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output:'export',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  trailingSlash: true
 };
 
 export default nextConfig;
